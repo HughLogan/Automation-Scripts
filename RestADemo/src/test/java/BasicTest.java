399ccfd48970751e240e45b866b0860395d23c3c
Manoj Kumar Olla <rveeranki@wsgc.com>
2020-03-10 03:17:03 -0700

    added sample test

---
+++ RestADemo/src/test/java/BasicTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasicTest {
+
+    @Test
+    public void getDetails() {
+        System.out.println("test >>>>>>>>>>.");
+    }
+}
