be799efdb2ad3064928a3f9d5ecc7fb9823a6eac
Manoj Kumar Olla <rveeranki@wsgc.com>
2020-03-10 04:17:12 -0700

    test1

--- RestADemo/src/test/java/BasicTest.java
+++ RestADemo/src/test/java/BasicTest.java
@@ -1,13 +1,16 @@
+import org.json.simple.parser.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class BasicTest {
 
     @Test
-    public void getDetails() {
+    public void getDetails() throws IOException, ParseException {
+       Basic.GetDetails();
         System.out.println("test >>>>>>>>>>.");
-        Assert.fail("FAILED");
     }
 }
